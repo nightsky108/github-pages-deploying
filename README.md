55773e783431a240700d60bc1529c8361c403b44
nightsky108 <softman009@outlook.com>
2016-12-15 13:04:58 -0600

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-github-pages-deploying
+## github-pages-deploying
 
+[Surge VS GitHub Pages: How to deploy a create-react-app project](https://medium.freecodecamp.com/surge-vs-github-pages-deploying-a-create-react-app-project-c0ecbf317089#.7afurn470)
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to github-pages-deploying</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This app is auto depolyed with create-react-app and gh-pages
         </p>
       </div>
     );
